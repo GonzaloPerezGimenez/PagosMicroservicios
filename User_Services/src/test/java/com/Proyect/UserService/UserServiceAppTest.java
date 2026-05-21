c0aef276b8cf1cbd228b09cdee209b6c70c2b039
Gonzalo.Perez <gonperezg@gmail.com>
2026-05-21 13:42:18 +0200

    Añadido test unitarios es UserServices

--- User_Services/src/test/java/com/Proyect/UserService/UserServiceAppTest.java
+++ User_Services/src/test/java/com/Proyect/UserService/UserServiceAppTest.java
@@ -1,5 +1,7 @@
 package com.Proyect.UserService;
 
+import java.math.BigDecimal;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -20,6 +22,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import com.Proyect.UserService.config.JwtServices;
 import com.Proyect.UserService.exceptions.UsernameAlreadyExist;
 import com.Proyect.UserService.model.User;
+import com.Proyect.UserService.model.UserResponseDTO;
 import com.Proyect.UserService.repository.UserRepository;
 import com.Proyect.UserService.service.UserService;
 
@@ -42,10 +45,8 @@ class UserServiceAppTest {
     @DisplayName("Test encriptacion de contraseña al registrar usuario")
     void saveUser_ShouldEncryptPassword() {
         // Arrange
-        User user = new User();
-        user.setUsername("testuser");
-        user.setPassword("plainPassword");
-        when(passwordEncoder.encode("plainPassword")).thenReturn("encryptedPassword");
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
+        when(passwordEncoder.encode(user.getPassword())).thenReturn("encryptedPassword");
         when(userRepository.save(any(User.class))).thenReturn(user);
 
         // When
@@ -61,7 +62,7 @@ class UserServiceAppTest {
     @DisplayName("Test para registrar un usuario")
     void saveUser() {
         // Arrange
-        User user = new User("testuser", "testUsername", "plainPassword");
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
 
         when(userRepository.save(user))
                 .thenReturn(user);
@@ -79,7 +80,7 @@ class UserServiceAppTest {
     @DisplayName("Test para validar que falla al registrar un usuario con username ya existente")
     void validateUserRegistration_Fails_WhenUsernameAlreadyExists() {
         // Arrange
-        User user = new User("testuser", "testUsername", "plainPassword");
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
 
         when(userRepository.findByUsername("testUsername"))
                 .thenReturn(Optional.of(user));
@@ -97,52 +98,206 @@ class UserServiceAppTest {
     @DisplayName("Test para devolver el token al iniciar sesión")
     void getAuthToken() {
         // Arrange
-        User user = new User();
-        String username = "testUsername";
-        String password = "plainPassword";
-        user.setUsername(username);
-        user.setPassword(password);
-        user.setId(1L);
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
 
-        when(userRepository.findByUsername(username))
+        when(userRepository.findByUsername(user.getUsername()))
                 .thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(password, password))
+        when(passwordEncoder.matches(user.getPassword(), user.getPassword()))
                 .thenReturn(true);
-        when(jwtServices.generateToken(username, 1L))
+        when(jwtServices.generateToken(user.getUsername(), 1L))
                 .thenReturn("AuthToken");
 
         //When
-        String token = userService.loginUser(username, password);
+        String token = userService.loginUser(user.getUsername(), user.getPassword());
 
         //Assert
         assertEquals("AuthToken", token);
     }
 
     @Test
-    @DisplayName("Test para devolver error al iniciar sesión si el usuario no existente o la contraseña es incorrecta")
-    void loginuser_Fails_WhenUserDoesNotExistOrPasswordIsIncorrect() {
-        // Arrange
-        User user = new User();
-        String username = "testUsername";
+    @DisplayName("Test para devolver error al iniciar sesión si el usuario no existente")
+    void loginuser_Fails_WhenUserDoesNotExist() {
+        // Arranque
         String usernameNotExist = "nonExistentUser";
-        String password = "plainPassword";
-        user.setUsername(username);
-        user.setPassword(password);
+
         when(userRepository.findByUsername(usernameNotExist))
                 .thenThrow(new IllegalArgumentException("Usuario no encontrado"));
-        when(userRepository.findByUsername(username))
-                .thenReturn(Optional.of(user));
-        when(passwordEncoder.matches(password, password))
-                .thenThrow(new IllegalArgumentException("Contraseña incorrecta"));
 
         //When
         IllegalArgumentException exceptionUsername = assertThrows(IllegalArgumentException.class, ()
-                -> userService.loginUser(usernameNotExist, password));
-        IllegalArgumentException exceptionPassword = assertThrows(IllegalArgumentException.class, ()
-                -> userService.loginUser(username, password));
+                -> userService.loginUser(usernameNotExist, "anyPassword"));
 
         //Assert
         assertEquals("Usuario no encontrado", exceptionUsername.getMessage());
+        verify(userRepository).findByUsername(usernameNotExist);
+        verify(passwordEncoder, never()).matches(any(String.class), any(String.class));
+        verify(jwtServices, never()).generateToken(any(String.class), any(Long.class));
+    }
+
+    @Test
+    @DisplayName("Test para devolver error al iniciar sesión si la contraseña es incorrecta")
+    void loginuser_Fails_WhenPasswordIsIncorrect() {
+        // Arrange
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
+
+        when(userRepository.findByUsername(user.getUsername()))
+                .thenReturn(Optional.of(user));
+        when(passwordEncoder.matches("PasswordIncorrecta", user.getPassword()))
+                .thenReturn(false);
+        //When
+        RuntimeException exceptionPassword = assertThrows(RuntimeException.class, ()
+                -> userService.loginUser(user.getUsername(), "PasswordIncorrecta"));
+        //Assert
         assertEquals("Contraseña incorrecta", exceptionPassword.getMessage());
+        verify(userRepository).findByUsername(user.getUsername());
+        verify(passwordEncoder).matches("PasswordIncorrecta", user.getPassword());
+        verify(jwtServices, never()).generateToken(any(String.class), any(Long.class));
+    }
+
+    @Test
+    @DisplayName("Test que devuelve un usuario al buscar por ID")
+    void getExistingUserById() {
+        //Arranque
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
+
+        when(userRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+
+        //When
+        UserResponseDTO userResponse = userService.getUserById(1L);
+
+        //Then
+        assertEquals("testUsername", userResponse.getUsername());
+        assertEquals(1L, userResponse.getId());
+        verify(userRepository).findById(1L);
+
+    }
+
+    @Test
+    @DisplayName("Test que devuelve error al buscar un usuario por username que no existe")
+    void validateToFindUserById_WhenUserDontExist() {
+        //Arranque
+        when(userRepository.findById(1L))
+                .thenThrow(new IllegalArgumentException("Usuario no encontrado"));
+
+        //When
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, ()
+                -> userService.getUserById(1L));
+
+        //Then
+        assertEquals("Usuario no encontrado", exception.getMessage());
+        verify(userRepository).findById(1L);
+    }
+
+    @Test
+    @DisplayName("Test que suma el monto al balance del usuario")
+    void creditUserBalance() {
+        //arranque
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
+        user.setBalance(BigDecimal.valueOf(100));
+        when(userRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+        //When
+        ResponseEntity<String> respuesta = userService.creditUserBalance(1L, BigDecimal.valueOf(100));
+
+        //Then
+        assertEquals("Cobro realizado con éxito.", respuesta.getBody());
+        assertEquals(BigDecimal.valueOf(200), user.getBalance());
+        verify(userRepository).findById(1L);
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    @DisplayName("Test que resta el monto al balance del usuario")
+    void debitUserBalance() {
+        //arranque
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
+        user.setBalance(BigDecimal.valueOf(100));
+        when(userRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+        //When
+        ResponseEntity<String> respuesta = userService.debitUserBalance(1L, BigDecimal.valueOf(100));
+
+        //Then
+        assertEquals("Pago realizado con éxito.", respuesta.getBody());
+        assertEquals(BigDecimal.valueOf(0), user.getBalance());
+        verify(userRepository).findById(1L);
+        verify(userRepository).save(user);
+    }
+
+    @Test
+    @DisplayName("Test que da error cuando resta el monto al balance del usuario y no hay saldo suficiente")
+    void validateToDebitUserBalance_WhenInsufficientBalance() {
+        //arranque
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
+        user.setBalance(BigDecimal.valueOf(10));
+        when(userRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+        //When
+        IllegalArgumentException excepcion = assertThrows(IllegalArgumentException.class, ()
+                -> userService.debitUserBalance(1L, BigDecimal.valueOf(100)));
+
+        //Then
+        assertEquals("Saldo insuficiente para realizar el débito.", excepcion.getMessage());
+        assertEquals(BigDecimal.valueOf(10), user.getBalance());
+        verify(userRepository).findById(1L);
+        verify(userRepository, never()).save(user);
+    }
+
+    @Test
+    @DisplayName("Test que actualiza la contraseña del usuario")
+    void updateUserPassword() {
+        //arranque
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
+        when(userRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+        when(passwordEncoder.encode("New_Pass"))
+                .thenReturn("Encoded_New_Pass");
+        //When
+        ResponseEntity<String> respuesta = userService.updateUser(1L, Map.of("password", "New_Pass"));
+        //Then
+        verify(userRepository).findById(1L);
+        verify(passwordEncoder).encode("New_Pass");
+        verify(userRepository).save(user);
+        assertEquals("Usuario actualizado con éxito.", respuesta.getBody());
+        assertEquals("Encoded_New_Pass", user.getPassword());
+    }
+
+    @Test
+    @DisplayName("Test que elimina un usuario")
+    void deleteUser() {
+        //arranque
+        User user = createUser(1L, "test_Name", "testUsername", "Test_Pass");
+        when(userRepository.findById(user.getId()))
+                .thenReturn(Optional.of(user));
+        //When
+        ResponseEntity<String> respuesta = userService.deleteUser(1L);
+        //Then
+        verify(userRepository).findById(1L);
+        verify(userRepository).delete(user);
+        assertEquals("Usuario eliminado con éxito.", respuesta.getBody());
+    }
+
+    @Test
+    @DisplayName("Test que da error al eliminar un usuario que no existe")
+    void validateToDeleteUser_WhenUserDoesNotExist() {
+        //arranque
+        when(userRepository.findById(1L))
+                .thenThrow(new IllegalArgumentException("Usuario no encontrado."));
+        //When
+        IllegalArgumentException excepcion = assertThrows(IllegalArgumentException.class, ()
+                -> userService.deleteUser(1L));
+
+        //Then
+        assertEquals("Usuario no encontrado.", excepcion.getMessage());
+        verify(userRepository).findById(1L);
+        verify(userRepository, never()).delete(any(User.class));
+    }
+
+    //Creacion de usuarios
+    private User createUser(Long id, String nombre, String username, String password) {
+        User user = new User(nombre, username, password);
+        user.setId(id);
+        return user;
     }
 }
